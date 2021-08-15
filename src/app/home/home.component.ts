6d9b8cd48454cf17aa45829a3609ccb5e5b064cd
Darlan-Freire <darlan.freirerj@gmail.com>
2021-08-15 09:55:08 -0300

    Create: ofertas.service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service'
+import { Oferta } from '../shared/oferta.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [ OfertasService ]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public ofertas: Array<Oferta>
+  
+  constructor(private ofertaService: OfertasService) {}
 
   ngOnInit(): void {
+    this.ofertas = this.ofertaService.getOfertas()
+    console.log(this.ofertas)
   }
 
 }
